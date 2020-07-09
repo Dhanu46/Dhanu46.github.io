70f0ac1ec819ce0169246b2abd9d9b6c7be1dc72
Dhanush <dhanu.r46@gmail.com>
2020-07-09 10:48:54 +0530

    little changes

--- script.js
+++ script.js
@@ -14,14 +14,14 @@ function startVideo() {
 video.addEventListener('playing', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
-  const displaySize = { width: (video.width/4), height: (video.height/4) }
+  const displaySize = { width: (video.width/8), height: (video.height/4) }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
- 
+
 
   }, 100)
 })
