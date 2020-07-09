4e2ad6e85b04f5de19188c9a9bf30b6fc9cf7c96
Dhanush Acharya <Dhanu.r46@gmail.com>
2020-07-09 10:34:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ video.addEventListener('playing', () => {
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+ 
+
   }, 100)
 })
