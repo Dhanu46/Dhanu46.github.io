c3976781ac37a524f9f10564e7906799d1016710
Dhanush Acharya <Dhanu.r46@gmail.com>
2020-07-09 10:15:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function startVideo() {
   )
 }
 
-video.addEventListener('play', () => {
+video.addEventListener('playing', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
